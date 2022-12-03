eed482b3f3cb80e8466e40fa379485db0654dc50
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-12-03 23:58:58 +0700

    done several steps needed to integrate with authentication API

---
+++ lib/page/authentication/LoginPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+  bool isPasswordVisible = false;
+  void togglePasswordView() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
+  String username = "";
+  String password1 = "";
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    // The rest of your widgets are down below
+
+    // TODO : Create login page
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Login'),
+      ),
+    );
+  }
+}
