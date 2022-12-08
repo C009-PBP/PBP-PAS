995d6a68a7d6249a943062fdb84bc550c4e4767c
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-12-09 05:10:08 +0700

    update data retrieval with user logged in filter

---
+++ lib/model/user.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+    User({
+        required this.username,
+        required this.role,
+        required this.pk,
+    });
+
+    String username;
+    String role;
+    int pk;
+
+    factory User.fromJson(Map<String, dynamic> json) => User(
+        username: json["username"],
+        role: json["role"],
+        pk: json["pk"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "username": username,
+        "role": role,
+        "pk": pk,
+    };
+}
--- lib/tools/fetcherBMI.dart
+++ lib/tools/fetcherBMI.dart
@@ -1,30 +1,59 @@
 //TODO: BUAT FETCHER JSON BMI
 //STATUS: (MUNGKIN) UDAH
-
+import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
+import 'package:healthbud/main.dart';
 
 import 'package:healthbud/model/bmi.dart';
 
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+
+import 'package:healthbud/page/authentication/LoginPage.dart';
+
 class fetcherBMI {
-  fetcherBMI ();
+  fetcherBMI();
 
   Future<List<BMI>> fetchBMI() async {
-    var url = Uri.parse(
-        'https://health-bud.up.railway.app/bmi_calculator/json/');
-    var response = await http.get(
-      url,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-    );
+    var response1;
+    try {
+      response1 = await request.get("http://localhost:8000/auth/user-data");
+      print(response1);
+    } catch (e) {
+      print("ERROr gan");
+    }
+    final user_pk = response1['pk'];
+    var url = Uri.parse('http://localhost:8000/bmi_calculator/json-flutter/${user_pk}');
 
+    var response;
+    try {
+      response = await http.get(
+        url,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      );
+      print("GGGGGGGGG");
+      print("ASOIPDJOSKAD");
+      // print((utf8.decode(response.bodyBytes)));
+    } catch (err) {
+      print("axz");
+    }
+
+    print("O");
     // melakukan decode response menjadi bentuk json
-    var data = jsonDecode(json.encode(utf8.decode(response.bodyBytes)));
+    // print(json.encode(response)); --> error
+    var data;
+    try {
+      data = jsonDecode(utf8.decode(response.bodyBytes)); //--> ERROR
+    } catch (e) {
+      print("askodaod");
+    }
+    print(data);
 
     // melakukan konversi data json menjadi object MyWatchList
     List<BMI> listBMI = [];
